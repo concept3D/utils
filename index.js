81f56635e22dc3d502437f8e7eccb8996ee4896f
Steve Wishstar <steve@concept3d.com>
2018-03-29 15:54:21 -0600

    Pulling in apiHelper functions.

--- index.js
+++ index.js
@@ -17,3 +17,31 @@ exports.connectDatabase = function() {
   return dynamoDb;
 }
 
+exports.dbError = function(err, req, res, next) {
+  const message = 'DB error. ';
+  console.log(message, err); // eslint-disable-line no-console
+  next(message);
+}
+
+exports.catchError = function(err, req, res, next) {
+  const message = 'Error caught in Space Planner API. (This has automatically been reported to our dev team.)';
+  /* eslint-disable no-console */
+  console.log(message);
+  console.log('err: ', err);
+  /* eslint-disable no-console */
+  next(message);
+}
+
+exports.stripLeadingTrailing = function(delim, string) {
+  let str = string.trim();
+  if (str.substr(0, delim.length) === delim) {
+    str = str.substring(delim.length);
+  }
+
+  const len = str.length;
+  if (str.substr(len - delim.length, delim.length) === delim) {
+    str = str.substring(0, len - delim.length);
+  }
+  return str;
+}
+
