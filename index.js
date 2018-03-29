4bec6f3f33bca36e43b14343a63a34dc8a5da8f6
Steve Wishstar <steve@concept3d.com>
2018-03-29 15:35:08 -0600

    adding db util

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+const AWS = require('aws-sdk');
+
+const { IS_OFFLINE, DB_PORT } = process.env;
+let dynamoDb;
+
+function connectDatabase() {
+  if (!dynamoDb) {
+    if (IS_OFFLINE === 'true') {
+      dynamoDb = new AWS.DynamoDB.DocumentClient({
+        region: 'localhost',
+        endpoint: `http://localhost:${DB_PORT}`,
+      });
+    } else {
+      dynamoDb = new AWS.DynamoDB.DocumentClient();
+    }
+  }
+  return dynamoDb;
 }
+
+exports.connectDatabase = connectDatabase();
 
